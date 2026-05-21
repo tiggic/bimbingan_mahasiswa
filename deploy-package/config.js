0285cee157442f15b4e48504d1b5879d6b6a7569
tiggic <161715733+tiggic@users.noreply.github.com>
2026-05-21 21:33:43 +0200

    Add files via upload

---
+++ deploy-package/config.js
@@ -0,0 +1,4 @@
+window.EPBS_SUPABASE_CONFIG = {
+  url: 'https://atfjykmbufktjjtcscds.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0Zmp5a21idWZrdGpqdGNzY2RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyODQ2MzYsImV4cCI6MjA5NDg2MDYzNn0.AJIuXP740wV9NedcdrPvfUbzoSD-tF2xTX3zg4Qv-OY'
+};
