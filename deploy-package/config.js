b7fd1bce56e7dc5ad9b4c090899fdb93a53f4faf
tiggic <161715733+tiggic@users.noreply.github.com>
2026-05-21 21:51:59 +0200

    Delete deploy-package directory

--- deploy-package/config.js
+++
@@ -1,4 +0,0 @@
-window.EPBS_SUPABASE_CONFIG = {
-  url: 'https://atfjykmbufktjjtcscds.supabase.co',
-  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF0Zmp5a21idWZrdGpqdGNzY2RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyODQ2MzYsImV4cCI6MjA5NDg2MDYzNn0.AJIuXP740wV9NedcdrPvfUbzoSD-tF2xTX3zg4Qv-OY'
-};
